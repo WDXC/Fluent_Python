d5b3c0cc5829628470353b8964c393676d331f2f
wdxc <1508498108zty@gmail.com>
2023-02-16 10:11:54 +0800

    update FrenchDeck2 in python 3.10 version

--- Chapter11/FrenchDeck2.py
+++ Chapter11/FrenchDeck2.py
@@ -1,8 +1,10 @@
 import collections
+from collections.abc import MutableSequence
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
-class FrenchDeck2(collections.MutableSequence):
+
+class FrenchDeck2(MutableSequence):
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spaces diamonds clubs hearts'.split()
 
